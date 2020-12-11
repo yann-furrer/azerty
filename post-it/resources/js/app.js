0b0a83e9be5ab6e9740c40bacc7c0688fb458a6f
yann <yann.furrer@epitech.digital>
2020-12-11 12:21:09 +0100

    m

---
+++ post-it/resources/js/app.js
@@ -0,0 +1,27 @@
+
+
+require('./bootstrap');
+import VueRouter from 'vue-router';
+import Home from './components/HomeComponent.vue';
+import Task from './components/TaskComponent.vue';
+window.Vue = require('vue');
+
+
+Vue.use(VueRouter);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+const routes = [
+{
+    path:'/home',
+    component: Home
+},
+{
+    path:'/task',
+    component: Task
+}
+]
+const router = new VueRouter({routes});
+const app = new Vue({
+    el: '#app',
+    router: router
+});
+
---
+++ post-it/resources/js/components/TaskComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <ul class="list-group">
+                <li class="list-group-item" v-for="task in tasks" :key="task.id"><a href="#">{{ task.name}}</a></li>
+             
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tasks:{}
+        }
+    },
+
+    created(){
+        axios.get('http://localhost:8000/taskList')
+        .then(response => this.tasks = response.data)
+        .catch(error => console.log(error));
+    },
+
+    mounted() {
+        console.log('Component mounted.')
+    }
+}
+</script>
